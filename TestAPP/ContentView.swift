6d18f206df8d21e7796d401b5bac70a336e7a90b
Yogesh <yogesh.sharma@mobileprogrammingllc.com>
2025-07-15 17:27:53 +0530

    Removed name

--- TestAPP/ContentView.swift
+++ TestAPP/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world! check name yogesh")
+            Text("Hello, world! check name ")
         }
         .padding()
     }
