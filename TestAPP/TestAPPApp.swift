d1f9f8b9ed48339986551a952bc4c078d21d979f
Yogesh <yogesh.sharma@mobileprogrammingllc.com>
2025-07-15 17:05:31 +0530

    Initial Commit

---
+++ TestAPP/TestAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAPPApp.swift
+//  TestAPP
+//
+//  Created by Yogesh on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct TestAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestAPPTests/TestAPPTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestAPPTests.swift
+//  TestAPPTests
+//
+//  Created by Yogesh on 7/15/25.
+//
+
+import Testing
+@testable import TestAPP
+
+struct TestAPPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
